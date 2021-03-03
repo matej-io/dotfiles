aaa137e26313554126dc3152fced9dc9bbf5da5e
Matej Ukmar <matejukmar@gmail.com>
2021-03-03 18:32:34 +0000

    Updates

--- fish/config.fish
+++ fish/config.fish
@@ -3,15 +3,7 @@ set fish_greeting "ready to roll!"
 set -g fish_user_paths "/usr/local/opt/python@3.9/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/bin" $fish_user_paths
 
-fish_vi_key_bindings
-for mode in insert default visual
-	bind -M $mode \cf forward-char
-end
-bind -M insert \; forward-char
-bind -M insert \' forward-char
-
 abbr -a l ls -al
 abbr -a s git status
-abbr -a c git commit -m\"
 abbr -a d cd ~/.config/dotfiles
 abbr -a m micro
