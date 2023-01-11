3bc2fbcec1c6b69d8c6f18dfbcd3399c0b30c606
Matej Ukmar <matejukmar@gmail.com>
2023-01-11 17:01:10 +0100

    updated rc files

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,7 @@ set fish_greeting "ready to roll!"
 
 set -g fish_user_paths "/usr/local/opt/python@3.9/bin" $fish_user_paths
 set -g fish_user_paths "$HOME/bin" $fish_user_paths
-set -g fish_user_paths "/Applications/Daedalus Mainnet.app/Contents/MacOS" $fish_user_paths
 set -g fish_user_paths "/Users/matej/Projects/Preview/depot_tools" $fish_user_paths
-
-set -x CARDANO_NODE_SOCKET_PATH /Users/matej/Library/Application Support/Daedalus Mainnet/cardano-node.socket
 
 abbr -a l ls -al
 abbr -a s sudo systemctl
@@ -13,5 +10,6 @@ abbr -a d cd ~/.config/dotfiles
 abbr -a m micro
 abbr -a art cd /Users/matej/Library/Mobile\ Documents/com~apple~CloudDocs/Art
 abbr -a g git push -u origin
+abbr -a p python3
 
 alias cpb="git branch --show-current | tr -d '\n' | pbcopy"
